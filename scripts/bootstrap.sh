b0055b4cd78f4636440e61d3240624430ee4f4e4
metal-face <hughesbryan3000@gmail.com>
2023-11-18 20:13:00 -0500

    bootstrap bash script written

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+if [ -z "$APP_ENV" ]; then
+    echo "Please set APP_ENV"
+    exit 1
+fi
+
+if [-z "$APP_COMPONENT"]; then
+    echo "Please set APP_COMPONENT"
+    exit 1
+fi
+
+# await database availability
+/scripts/await-service.sh $DATABASE_HOST $DATABASE_PORT $SERVICE_READINESS_TIMEOUT
+
+# run the service
+/usr/localbin/video-editor-api
