cda4daf785d8efa29ab9858ae67946fcf4cf7787
metal-face <hughesbryan3000@gmail.com>
2023-11-20 14:37:00 -0500

    syntax error fix

--- scripts/await-service.sh
+++ scripts/await-service.sh
@@ -3,18 +3,18 @@ set -eo pipefail
 
 await_service()
 {
-    local start_ts = $(date +%s)
+    local start_ts=$(date +%s)
     while [ $(date +%s) -lt $((start_ts + $3)) ];
     do
         (echo -n > /dev/tcp/$1/$2) > /dev/null
-        if [[ $? -eq 0]]; then
+        if [[ $? -eq 0 ]]; then
             break
         fi
         sleep 1
     done
-    local end_ts = $(date +%s)
+    local end_ts=$(date +%s)
 
-    if [ $(date +%s) -ge $((start_ts + $3))]; then
+    if [ $(date +%s) -ge $((start_ts + $3)) ]; then
         echo "Timeout occurred while waiting for $1:$2 to become available"
         exit 1
     fi
@@ -22,7 +22,7 @@ await_service()
     echo "$1:$2 is available after $((end_ts - start_ts)) seconds"
 }
 
-if [[ $# -ne 3]]; then
+if [[ $# -ne 3 ]]; then
     echo "Usage: $0 <host> <port> <timeout>"
     exit 1
 fi
