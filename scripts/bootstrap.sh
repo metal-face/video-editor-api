8ff909ae621bf037d907c56c5e401a40971127cf
metal-face <hughesbryan3000@gmail.com>
2023-11-22 03:12:28 -0500

    Fix script bootstrap.sh and update service path

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -e
 
 if [ -z "$APP_ENV" ]; then
     echo "Please set APP_ENV"
     exit 1
 fi
 
-cd /srv/root
+
 
 if [ -z "$APP_COMPONENT" ]; then
     echo "Please set APP_COMPONENT"
@@ -15,13 +15,6 @@ fi
 
 # await database availability
 /scripts/await-service.sh $DATABASE_HOST $DATABASE_PORT $SERVICE_READINESS_TIMEOUT
-
 /scripts/init-db.sh
 
-#  TODO: run migrations
-diesel migration run
-
-cargo run
-
-# run the service
-# /usr/localbin/video-editor-api
+/usr/local/bin/video-editor-api
