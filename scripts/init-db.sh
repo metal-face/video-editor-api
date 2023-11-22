8d99aa16611805f56cab36a0c4e5013f3a73ca12
metal-face <hughesbryan3000@gmail.com>
2023-11-21 22:25:27 -0500

    Update database connection parameters

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -2,11 +2,11 @@
 set -e
 
 execDBStatement() {
-    echo "$1" | PGPASSWORD=$DB_PASS psql \
-        --host=$DB_HOST \
-        --port=$DB_PORT \
-        --username=$DB_USER \
-        --dbname=$DB_NAME \
+    echo "$1" | PGPASSWORD=$DATABASE_PASS psql \
+        --host=$DATABASE_HOST \
+        --port=$DATABASE_PORT \
+        --username=$DATABASE_USER \
+        --dbname=$DATABASE_NAME \
 }
 
 execDBStatement "SELECT 'CREATE DATABASE ${DB_NAME}' WHERE NOT EXISTS (SELECT FROM ${DB_NAME} WHERE datname = '${DB_NAME}')\gexec"
